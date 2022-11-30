2e8a6c54a40f02a0bf72665089e7a683d18374bc
maxim <shatunov-87@mail.ru>
2022-11-30 23:13:33 +0300

    Dz7

---
+++ 50/Program.cs
@@ -0,0 +1,70 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет*/
+
+using static System.Console;
+Clear();
+
+
+WriteLine("Введите число: ");
+int namber = int.Parse(ReadLine());
+
+int[,] mat = GetMatrix(4, 4);
+
+PrintMatrix(mat);
+
+
+int[,] GetMatrix(int rows, int colums)
+{
+    int[,] result = new int[rows, colums];
+  
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            result[i, j] = new Random().Next(0,10);
+        }
+    }
+    return result;
+
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Write($"{matrix[i, j]}\t ");
+        }
+        WriteLine();
+    }
+
+}
+
+
+for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            if (mat[i,j] == namber) 
+            {
+             
+            
+             Write($"значение этого элемента ({(i)},{(j)})");
+             break;
+             
+            }
+        }
+        
+        
+    } 
+
+     
+        
---
+++ 52/Program.cs
@@ -0,0 +1,47 @@
+﻿//адача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+// 1 4 7 2                                  
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+using static System.Console;
+Clear();
+
+
+WriteLine("Введите количество строк m: ");
+int rows= int.Parse(ReadLine());
+WriteLine("Введите количество столбцов n: ");
+int colums = int.Parse(ReadLine());
+
+int[,] array = new int[rows, colums];
+    
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            array[i, j] =  new Random().Next(0,10);
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            Write($"{array[i, j]}\t ");
+        }
+        WriteLine();
+    }
+
+for (  int j=0; j < colums;j++)
+{
+    double summ = 0;
+
+     for (int i = 0; i < rows;i++)
+    summ+=array[i,j];
+    Console.WriteLine($"среднее арифметическое {(j+1)} столбца = {(summ/rows)}");
+     }
+      
